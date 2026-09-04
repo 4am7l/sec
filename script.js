405138a1964d386e45ec1e5dce5b7631d13e0281
Mohammad Janaideh <mhmdjnaydt515@gmail.com>
2026-09-04 14:47:18 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('mainBtn');
-const msg = document.getElementById('message');
-
-btn.addEventListener('click', () => {
-    msg.textContent = 'Action triggered successfully!';
-});
